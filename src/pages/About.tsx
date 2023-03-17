459c95327289797d7d0f494ead4681768d21f53a
ssekridov@gmail.com <ssekridov@gmail.com>
2023-03-17 21:42:08 +0100

    refactor: more headers

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 
 function About() {
   return (
-    <>
+    <header>
       <h1>About us</h1>
       <Link to="/">Home</Link>
-    </>
+    </header>
   );
 }
 export default About;
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 
 function NotFound() {
   return (
-    <>
+    <header>
       <h1>No such page</h1>
       <Link to="/">Home</Link>
-    </>
+    </header>
   );
 }
 export default NotFound;
