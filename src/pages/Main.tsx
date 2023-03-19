3372cabd79b1006f81a1f84b56a1dce577e185cb
ssekridov@gmail.com <ssekridov@gmail.com>
2023-03-19 11:30:04 +0100

    feat: searchbar value to LS and back

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,18 @@
 import { Link } from 'react-router-dom';
+import { useState, ChangeEvent } from 'react';
+import getLSValue from '../components/getLSValue';
+import setLSValue from '../components/setLSValue';
 import Spellcards from '../components/Spellcards';
 
 function Main() {
+  const [message, setMessage] = useState('');
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setMessage(event.target.value);
+    setLSValue('spell', event.target.value);
+    return message;
+  };
+
   return (
     <>
       <header>
@@ -10,7 +21,12 @@ function Main() {
       </header>
       <body>
         <fieldset className="searchfield">
-          <input type="search" placeholder="search spells" />
+          <input
+            type="search"
+            placeholder="search spells"
+            onChange={handleChange}
+            defaultValue={getLSValue('spell')}
+          />
           <button type="button">Search</button>
         </fieldset>
         <Spellcards />
