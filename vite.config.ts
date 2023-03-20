a43d6ad3d27ca70df9588275a0c905aa661d2ee2
ssekridov@gmail.com <ssekridov@gmail.com>
2023-03-20 14:06:37 +0100

    feat: test coverage setup

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,9 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'html'],
+    },
   },
 });
