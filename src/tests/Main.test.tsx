d586f5aae2758d0bb409db72b3227df59e73896a
ssekridov@gmail.com <ssekridov@gmail.com>
2023-04-02 16:09:43 +0200

    feat: spellcards number test

--- src/tests/Main.test.tsx
+++ src/tests/Main.test.tsx
@@ -1,10 +1,16 @@
 import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { WrappedApp } from '../App';
+import spells from '../components/spells';
 
 describe('Main', () => {
   it('Renders Spellcards', () => {
     render(<WrappedApp />);
     expect(screen.getByRole('list')).toBeInTheDocument();
+  });
+  it('Displays the correct number of spellcards', async () => {
+    render(<WrappedApp />);
+    const listItems = await screen.findAllByRole('listitem');
+    expect(listItems).toHaveLength(spells.length);
   });
 });
